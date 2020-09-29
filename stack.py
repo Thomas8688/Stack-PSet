62822d8407acbc4aae66db75fb3377ce6ed74c88
Thomas Eley <thomas-eley@hotmail.co.uk>
2020-09-29 23:02:15 +0400

    Stack Push

--- stack.py
+++ stack.py
@@ -3,6 +3,7 @@ class stack():
 #Initialisation
     def __init__(self, size):
 #If the input for stack size is not an integer, the size will be defaulted to 10
+        print("\nCreating Stack")
         if isinstance(size, int):
             self.__size = size
         else:
@@ -10,12 +11,14 @@ class stack():
             print("Invalid Stack Size: Set to Default (10)")
 #Sets the stack to an empty list
         self.__stack = []
+        print("Stack Created\n")
 
 #Procedure used to add an input URL to the stack
     def push(self, URL):
         if isinstance(URL, str):
             if not self.isFull():
                 self.__stack.append(URL)
+                print(URL, "has been Added to the Stack")
             else:
                 print("Stack is Full: Cannot push", URL)
         else:
@@ -24,7 +27,7 @@ class stack():
 #Function used to remove the top item from the stack and return it to the user
     def popTop(self):
         if self.isEmpty():
-            print("The Stack is Empty")
+            print("The Stack is Empty: Cannot Pop")
         else:
             top = self.__stack[-1]
             self.__stack = self.__stack[:-1]
@@ -33,9 +36,9 @@ class stack():
 #Function used to print the top item in the stack
     def peek(self):
         if self.isEmpty():
-            print("The Stack is Empty")
+            print("The Stack is Empty: Cannot Peek")
         else:
-            print(self.__stack[-1])
+            print("Top of stack:", self.__stack[-1])
 
 #Function used to check if the stack is empty
     def isEmpty(self):
@@ -52,12 +55,35 @@ class stack():
             return False
 
 #TESTING
+#Creates new stack
 myStack = stack(3)
+#Adds 3 URLs to Stack
 myStack.push("afl.com.au")
 myStack.push("theage.com.au")
 myStack.push("hawthornfc.com.au")
+#Removes the top item from the stack and prints it
 lastPage = myStack.popTop()
 print(lastPage)
+#Prints the NEW top value of the stack
 myStack.peek()
+#Adds another item to the stack
 myStack.push("outlook.com")
+#Prints the NEW top value of the stack
 myStack.peek()
+#Error Handling TESTING
+myStack2 = stack("hello")
+myStack2.push(16)
+myStack2.peek()
+shouldBeEmpty = myStack2.popTop()
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+myStack2.push("afl.com.au")
+print("Test")
